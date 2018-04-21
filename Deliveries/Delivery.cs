472e2d4707a7bf7a26764f38d96dee1722fe601d
NotedToSelf <notedtoself@gmail.com>
2018-04-21 01:33:03 -0700

    Consolidated some files. Possibly not convention?

--- Deliveries/Delivery.cs
+++ Deliveries/Delivery.cs
@@ -14,6 +14,57 @@ namespace Deliveries
         
     }
 
-  
+    //Standard delivery class
+    //Max capacity: 20
+    //Is not concerned with optimal route
+    class Standard : Delivery
+    {
+        public Standard()
+        {
+            capacity = 20;
+            estimate = 0.0f;
+            payment = 0.0f;
+        }
+        
+        public override int deliver()
+        {
+            return 0;
+        }
+    }     
     
+    //Express delivery class
+    //Max capacity: 3
+    //Must find optimal route
+    class Express : Delivery
+    {
+        public Express()
+        {
+            capacity = 3;
+            estimate = 0.0f;
+            payment = 0.0f;
+        }
+        
+        public override int deliver()
+        {
+            return 0;
+        }
+    }
+    
+    //Drone delivery class
+    //Max capacity: 1
+    //Delivers straight from one location to another
+    class Drone : Delivery
+    {
+        public Drone()
+        {
+            capacity = 1;
+            estimate = 0.0f;
+            payment = 0.0f;
+        }
+
+        public override int deliver()
+        {
+            return 0;
+        }
+    }
 }
